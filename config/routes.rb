8434955113decf2fa7a6f889f27b78587dbd5d3b
Trystan Newbury trystan.newbury@gmail.com <trystan.newbury@gmail.com>
2017-09-05 16:15:47 +0200

    Rails Task Manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+
+  get 'tasks', to: 'tasks#index'
+
+  post 'tasks', to: 'tasks#create'
+
+  get 'tasks/new', to: 'tasks#new', as: 'new_task'
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+  patch 'tasks/:id', to: 'tasks#update'
+
+  delete 'tasks/:id', to: 'tasks#destroy'
+
+  # resources :tasks
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
